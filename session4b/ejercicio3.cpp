ca12e3574e8b9328abfba92bc5776ae1de366297
Alvaro Kumar Garcia Agrawal <alvaro.garcia.a@utec.edu.pe>
2019-09-12 10:56:23 -0500

    Ejercicio 3

--- session4b/ejercicio3.cpp
+++ session4b/ejercicio3.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
+    char carac="a";
+    int i=4;
+    char *p=&carac;
+    char **pp=&p;
+    int *p_entero=&i;
+    int **pp_entero=p_entero;
     return 0;
 
 }
